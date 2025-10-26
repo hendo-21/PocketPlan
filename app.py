492ae8308973bdc07ec37ace82c9692a94ea5a1e
hendo-21 <ian.james.henderson@gmail.com>
2025-10-26 15:51:06 -0700

    Refactoring. Moved model creation into main app file.

--- app.py
+++ app.py
@@ -1,24 +1,35 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy     # Flask-SQLAlchemy extension initialization
 from sqlalchemy.orm import DeclarativeBase  # Flask-SQLAlchemy extension initialization
+from sqlalchemy.orm import Mapped, mapped_column
+from datetime import date
 
-# Initialize the Flask-SQLAlchemy extension
+"""
+Database config:
+- Initialize the Flask-SQLAlchemy extension
+- Gives access to db.Model class to define models, and db.session to execute queries
+- Create the app
+- sqlite:// indicates it's a SQLite database | transactions.db is the name of the SQLite db file. File location is a relative path: project.db located in same dir as app.py
+"""
 class Base(DeclarativeBase):
     pass
-
-# Gives access to db.Model class to define models, and db.session to execute queries
 db = SQLAlchemy(model_class=Base)
-
-# Creates the app
 app = Flask(__name__)
-
-# sqlite:// indicates it's a SQLite database | project.db is the name of the SQLite db file
-# File location is a relative path: project.db located in same dir as app.py
-app.config["SQALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///transactions.db"
 db.init_app(app)
 
-# Import database models
-from . import models
+# Define database model
+class Transactions(db.Model):
+    # Set the name of the table
+    __tablename__ = "transactions"
+
+    # Create column for unique ID - needed/highly recommended
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    # Create columns for settings, defaulting to $0 
+    date: Mapped[str] = mapped_column(default=date.today().strftime("%D"))
+    amount: Mapped[float] = mapped_column(default=0.0)
+    memo: Mapped[str] = mapped_column()
 
 # Create models and tables - update this later to a database migration tool
 with app.app_context():
@@ -28,3 +39,5 @@ with app.app_context():
 @app.route("/")
 def index():
     return render_template('index.html')
+
+print("is this thing on?")
--- models.py
+++ models.py
@@ -1,12 +1,14 @@
+# 10/26/25 - Leaving this in repo for now, but will delete later. Just one model needed so leaving model definition in main app file. Addresses circular import issue.
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import Mapped, mapped_column
-from .app import db
+from app import db
 
-class Settings(db.Model):
+class Budget(db.Model):
     # Set the name of the table
-    __tablename__ = "settings"
+    __tablename__ = "budget"
 
-    # Create column for unique ID - needed
+    # Create column for unique ID - needed/highly recommended
     id: Mapped[int] = mapped_column(primary_key=True)
 
     # Create columns for settings, defaulting to $0 
