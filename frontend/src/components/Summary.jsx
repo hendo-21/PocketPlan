a46f3e4f6d81409f1a3609e6f310ffa004d07da1
hendo-21 <ian.james.henderson@gmail.com>
2025-12-29 12:07:34 -0800

    Added date parsing to Summary and TransactionTableRow components.

--- frontend/src/components/Summary.jsx
+++ frontend/src/components/Summary.jsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
+import { parseISO, format } from "date-fns";
 
 export default function Summary({ summary, totalSpent }) {
     // Guard against slow return of db object
     if(!summary) {
         return <div>Summary loading...</div>;
     }
+
+    // Convert last_updated to mm/dd/yy format
+    const last_updatedDateObj = parseISO(summary.last_updated)
+    const formattedLastUpdatedDate = format(last_updatedDateObj, 'MM/dd/yy')
     
     // Prepare state
     const [budget, setBudget] = useState(summary.budget)
-    const [last_updated, setLastUpdated] = useState(summary.last_updated)
+    const [last_updated, setLastUpdated] = useState(formattedLastUpdatedDate)
 
     return (
         <div className="summary">
--- frontend/src/components/TransactionTableRow.jsx
+++ frontend/src/components/TransactionTableRow.jsx
@@ -1,9 +1,16 @@
 import { MdOutlineEditNote } from "react-icons/md";
+import { parseISO, format } from 'date-fns';
 
 export default function TransactionTableRow({ transaction }) {
+    // Convert date ISO to a Date object
+    const dateObj = parseISO(transaction.date_added)
+
+    // Format the Date object
+    const formattedDate = format(dateObj, 'MM/dd/yy')
+
     return (
         <tr>
-            <td>{transaction.date}</td>
+            <td>{formattedDate}</td>
             <td>${transaction.amount}</td>
             <td>{transaction.memo}</td>
             <td><MdOutlineEditNote/></td>
